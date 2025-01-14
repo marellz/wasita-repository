e1387ca35f236cbe1204bf209db6c6d6af9d7c03
Dave Njoroge <davenjoroge10@gmail.com>
2025-01-14 20:05:04 +0300

    supabase cli, update; added moment

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-import type { Database } from './types'
+import type { Database } from '../types/supabase'
 
 const url = import.meta.env.VITE_SUPABASE_URL
 const anonKey = import.meta.env.VITE_SUPABASE_PUBLIC_KEY
