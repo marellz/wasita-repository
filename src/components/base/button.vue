532120f49a64ce75dace4a477531edaab79f9ced
Dave Njoroge <davenjoroge10@gmail.com>
2025-01-14 14:26:46 +0300

    loader update

--- src/components/base/button.vue
+++ src/components/base/button.vue
@@ -1,15 +1,16 @@
 <template>
   <!--<button class="inline-flex space-x-2 text-sm font-medium px-2 py-1 rounded" :type
     :disabled="loading || disabled"> -->
-  <button class="px-4 text-white font-medium py-2 rounded-lg transition-colors border border-transparent" :type
-    :class="themes[variant]" :disabled="loading || disabled">
+  <button
+    class="px-4 text-white font-medium py-2 rounded-lg transition-colors border border-transparent inline-flex justify-center relative"
+    :type :class="themes[variant]" :disabled="loading || disabled">
     <slot />
-    <custom-loader v-if="loading" />
+    <base-loader class="absolute right-2" v-if="loading" />
   </button>
 </template>
 <script lang="ts" setup>
+import BaseLoader from '@/components/base/loader.vue'
 type BtnVariants = "primary" | "secondary" | "primary-outline" | "secondary-outline"
-
 type VariantThemes = {
   [key in BtnVariants]: string;
 }
@@ -26,7 +27,7 @@ withDefaults(
 );
 
 const themes: VariantThemes = {
-  "primary": "bg-indigo-600 hover:bg-indigo-700",
+  "primary": "bg-indigo-600 hover:bg-indigo-700 disabled:bg-gray-700",
   "primary-outline": "border-indigo-600 text-indigo-600",
   "secondary": "bg-gray-800 hover:bg-indigo-800",
   "secondary-outline": "border-gray-600 text-gray-800",
--- src/components/base/loader.vue
+++ src/components/base/loader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="loader">
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
       stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
       <path d="M21 12a9 9 0 1 1-6.219-8.56"></path>
@@ -16,18 +16,18 @@ withDefaults(
   }
 );
 </script>
-<style lang="scss" scoped>
+<style scoped>
 .loader {
   animation: loader 0.75s linear infinite;
 }
 
 @keyframes loader {
   from {
-    transform: rotate(360deg);
+    transform: rotate(0deg);
   }
 
   to {
-    transform: rotate(0deg);
+    transform: rotate(360deg);
   }
 }
 </style>
