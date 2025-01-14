35d217086ac9d75a9aeb10223a5462acd0f6781a
Dave Njoroge <davenjoroge10@gmail.com>
2025-01-14 20:10:18 +0300

    about page

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,5 +1,44 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <Container>
+    <div class="">
+      <div class="max-w-4xl mx-auto">
+        <img src="@/assets/images/about.jpg" alt="" class="rounded-[100px]">
+        <h1 class="text-[60px] mb-10 mt-10">About.</h1>
+        <div class="space-y-6 text-xl leading-relaxed">
+          <p>This repository is the brainchild of our family’s collaborative spirit, created and managed with care and
+            precision.
+            While I oversee its development and maintenance, the idea was sparked by my uncle Stephen and strongly
+            supported
+            by
+            my uncle John. Together, we recognized the need for a reliable, centralized space to store and manage
+            essential
+            documents for our new self-help group. This initiative reflects the collective effort of our family to stay
+            organized and move forward efficiently.</p>
+          <p> The need for this repository became evident as important files and discussions often got lost or buried in
+            the
+            flurry of WhatsApp conversations. By creating a dedicated system, we aim to eliminate confusion, ensure
+            everyone
+            can
+            access critical resources when needed, and track responsibilities with clarity. This repository is the first
+            step
+            in
+            what we envision as a series of tools to enhance our group’s operations and foster smoother collaboration.
+          </p>
+          <p>
+            At its core, this repository stands on values that define our family: trust, reliability, and accessibility.
+            Guided
+            by honesty, pragmatism, and meritocracy, we strive to create a space that not only meets practical needs but
+            also
+            upholds the shared principles that bind us together. This is more than a storage solution; it’s a testament
+            to
+            our
+            commitment to building a better, more organized future for our self-help group.</p>
+        </div>
+      </div>
+    </div>
+  </Container>
 </template>
+<script setup lang="ts">
+import Container from '@/components/layout/container.vue';
+
+</script>
