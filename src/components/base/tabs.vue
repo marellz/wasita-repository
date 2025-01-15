a5872a6f2f9c89f34cbaa206035a883818ddee3a
Dave Njoroge <davenjoroge10@gmail.com>
2025-01-15 17:51:22 +0300

    update loader, tabs components

---
+++ src/components/base/tabs.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex">
+    <ul class="border border-slate-400 p-2 rounded-lg flex">
+      <li v-for="{ label, key } in tabs" :key="key">
+        <a :href="`#${key}`" class="text-slate-500 text-sm px-3 py-1.5 font-medium hover:bg-slate-100 rounded"
+          :class="{ '!text-black': active === key }" @click.prevent="active = key">
+          {{ label }}
+        </a>
+      </li>
+    </ul>
+
+  </div>
+  <div class="mt-10">
+    <slot :name="active"></slot>
+  </div>
+</template>
+<script lang="ts" setup>
+import { onMounted, ref, watch } from 'vue';
+
+interface Tab {
+  key: string,
+  label: string;
+}
+
+const props = defineProps<{
+  tabs: Tab[],
+  default?: string
+}>()
+
+const active = ref<string>(props.default ?? props.tabs[0].key)
+
+const emit = defineEmits(['change']);
+
+watch(active, (v) => {
+  emit('change', v)
+})
+
+onMounted(() => {
+  if (!props.default && props.tabs.length) {
+    active.value = props.tabs[0].key
+  }
+})
+</script>
