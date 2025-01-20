0c37d5d38bfece2d2ef2ece312216eb1b8aff1e0
Dave Njoroge <davenjoroge10@gmail.com>
2025-01-20 22:51:30 +0300

    update auth validation

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,43 +1,87 @@
 <template>
   <div>
     <h1 class="text-3xl font-bold mb-6">Login</h1>
-    <form @submit.prevent="login">
+    <Form @submit="login()">
       <div class="space-y-4">
-        <form-input label="Email address" v-model="user.email" type="email" required></form-input>
-        <form-input label="Password" v-model="user.password" type="password" required></form-input>
-        <div class="flex flex-col items-start md:flex-row md:items-center space-y-4 md:space-y-0 justify-between">
+        <form-input
+          label="Email address"
+          v-model="email"
+          :error="errors.email"
+          type="email"
+          required
+        ></form-input>
+        <form-input
+          label="Password"
+          v-model="password"
+          :error="errors.password"
+          type="password"
+          required
+        ></form-input>
+        <div
+          class="flex flex-col items-start md:flex-row md:items-center space-y-4 md:space-y-0 justify-between"
+        >
           <form-checkbox v-model="rememberMe" label="Remember me" />
-          <router-link class="text-indigo-600 font-medium" to="/forgot-password">Forgot password? </router-link>
+          <router-link class="text-indigo-600 font-medium" to="/forgot-password"
+            >Forgot password?
+          </router-link>
         </div>
         <base-button class="w-full" :loading="auth.loading">
           <span>Login</span>
         </base-button>
+        <base-alert
+          v-if="auth.hasErrors"
+          variant="error"
+          title="Authentication error"
+        >
+          <p v-for="(err, key) in auth.errors" :key>
+            <span class="font-bold">{{ key }}</span
+            >: <span>{{ err }}</span>
+          </p></base-alert
+        >
         <div class="space-y-2">
-          <p class="text-gray-600 text-center">Dont have an account? <router-link class="text-indigo-600 font-medium"
-              to="/register">Register</router-link>
+          <p class="text-gray-600 text-center">
+            Dont have an account?
+            <router-link class="text-indigo-600 font-medium" to="/register"
+              >Register</router-link
+            >
           </p>
         </div>
       </div>
-    </form>
+    </Form>
   </div>
 </template>
 <script lang="ts" setup>
-import FormInput from '@/components/form/input.vue';
-import FormCheckbox from '@/components/form/checkbox.vue';
-import BaseButton from '@/components/base/button.vue'
-import { ref } from 'vue';
-import { useAuthStore } from '@/stores/auth';
-
+import FormInput from "@/components/form/input.vue"
+import FormCheckbox from "@/components/form/checkbox.vue"
+import BaseButton from "@/components/base/button.vue"
+import BaseAlert from "@/components/base/alert.vue"
+import { ref } from "vue"
+import { useAuthStore } from "@/stores/auth"
+import { Form, useForm } from "vee-validate"
+import * as yup from "yup"
 const auth = useAuthStore()
 
-const user = ref({
-  email: "", //dave@test.com
-  password: "", //dave123@dave
+// const user = ref({
+//   email: "", //dave@test.com
+//   password: "", //dave123@dave
+// })
+
+const { errors, defineField, handleSubmit } = useForm({
+  validationSchema: yup.object({
+    email: yup.string().email().required("Email is required"),
+    password: yup.string().min(6).required("Password is required"),
+  }),
 })
+
+const [email] = defineField("email")
+const [password] = defineField("password")
 
 const rememberMe = ref(false)
 
-const login = async () => {
-  await auth.login(user.value)
-}
+const login = handleSubmit(async (values) => {
+  await auth.login({
+    email: values.email,
+    password: values.password,
+  })
+})
 </script>
--- src/views/auth/register.vue
+++ src/views/auth/register.vue
@@ -1,45 +1,114 @@
 <template>
-
   <h1 class="text-2xl font-bold mb-6">Register</h1>
-  <Form @submit.prevent="register">
+  <Form @submit="register()" @invalid-submit="handleInvalidSubmit">
     <div class="space-y-4">
-      <form-input name="name" label="Name" v-model="user.name" required></form-input>
-      <form-input name="email" label="Email" :error="errors.email" v-model="user.email" type="email" required></form-input>
-      <form-input name="password" label="Password" v-model="user.password" type="password" required></form-input>
-      <form-input name="passwordConfirmation" label="Confirm password" v-model="user.confirmPassword" type="password" required></form-input>
+      <form-input
+        label="Name"
+        v-model="name"
+        required
+        :error="errors.name"
+        v-bind="nameAttrs"
+      ></form-input>
+
+      <form-input
+        label="Email"
+        :error="errors.email"
+        v-model="email"
+        type="email"
+        required
+      ></form-input>
+
+      <form-input
+        label="Password"
+        v-model="password"
+        type="password"
+        required
+        :error="errors.password"
+      ></form-input>
+
+      <form-input
+        label="Confirm password"
+        v-model="confirmPassword"
+        type="password"
+        required
+        :error="errors.password_confirmation"
+      ></form-input>
+
       <div>
         <base-button class="w-full" :loading>
           <span>Register</span>
         </base-button>
       </div>
+      <base-alert
+        v-if="auth.hasErrors"
+        variant="error"
+        title="Authentication error"
+      >
+        <div class="space-y-2">
+          <p v-for="(err, key) in auth.errors" :key>
+            <span>{{ err }}</span>
+          </p>
+        </div>
+      </base-alert>
       <div>
-        <p class="text-gray-600 text-center">Already have an account? <router-link class="text-indigo-600 font-medium"
-            to="/login">Login</router-link>
+        <p class="text-gray-600 text-center">
+          Already have an account?
+          <router-link class="text-indigo-600 font-medium" to="/login"
+            >Login</router-link
+          >
         </p>
       </div>
     </div>
   </Form>
 </template>
 <script lang="ts" setup>
-import { Form } from 'vee-validate';
-import FormInput from '@/components/form/input.vue';
-import BaseButton from '@/components/base/button.vue'
-import { computed, ref } from 'vue';
-import { useAuthStore } from '@/stores/auth';
+import { Form, useForm } from "vee-validate"
+import FormInput from "@/components/form/input.vue"
+import BaseButton from "@/components/base/button.vue"
+import BaseAlert from "@/components/base/alert.vue"
+import { computed } from "vue"
+import { useAuthStore } from "@/stores/auth"
+
+import * as yup from "yup"
+
+const schema = yup.object({
+  name: yup.string().required("Name is required"),
+  email: yup.string().email().required("Email is required"),
+  password: yup.string().min(6).required("Password is required"),
+  password_confirmation: yup
+    .string()
+    .required()
+    .oneOf([yup.ref("password")], "Passwords do not match"),
+})
+
+const { errors, defineField, handleSubmit } = useForm({
+  validationSchema: schema,
+})
 
 const auth = useAuthStore()
 
-const errors = computed(() => auth.errors)
 const loading = computed(() => auth.loading)
 
-const user = ref({
-  name: "", //Dave Njoroge
-  email: "", //dave@test.com
-  password: "", //dave123@dave
-  confirmPassword: "", //dave123@dave
+const [name, nameAttrs] = defineField("name")
+const [email] = defineField("email")
+const [password] = defineField("password")
+const [confirmPassword] = defineField("password_confirmation")
+
+const register = handleSubmit(async (values) => {
+  console.log(values)
+
+  await auth.register({
+    name: values.name,
+    email: values.email,
+    password: values.password,
+    confirmPassword: values.confirmPassword,
+  })
 })
 
-const register = async () => {
-  await auth.register(user.value)
+const handleInvalidSubmit = () => {
+  if (Object.keys(errors).length) {
+    alert("Errors!")
+    return null
+  }
 }
 </script>
