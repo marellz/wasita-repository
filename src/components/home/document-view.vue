ca37a9a8f6eea369fa643d9a7567a4aad170a704
Dave Njoroge <davenjoroge10@gmail.com>
2025-01-23 21:30:32 +0300

    docs component update

--- src/components/home/document-view.vue
+++ src/components/home/document-view.vue
@@ -19,7 +19,11 @@
             <base-button @click="openDocument(document.url, document.id)">
               <span>View document</span>
             </base-button>
-            <base-button v-if="auth.user.id === document.user_id" variant="danger" @click="deleteDocument(document.id)">
+            <base-button
+              v-if="auth.user.id === document.user_id"
+              variant="danger"
+              @click="deleteDocument(document.id)"
+            >
               <span>Delete document</span>
             </base-button>
           </div>
@@ -28,12 +32,23 @@
       <div>
         <p class="text-sm text-gray-500 mb-2">Authored by</p>
         <div class="flex space-x-2 items-center">
-          <img class="h-12 w-12 object-center rounded-full border-2"
-            src="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/09/09834fb098969556fda54d497357ad20f46a6849_full.jpg"
-            alt="" />
+          <img
+            v-if="document.user.avatar_url"
+            class="h-16 w-16 object-center rounded-full border-2"
+            :src="document.user.avatar_url"
+            alt=""
+          />
+          <span
+            v-else
+            class="h-16 w-16 object-center rounded-full border-2 bg-slate-200 inline-flex items-center justify-center"
+          >
+            <User2 :size="40" :stroke-width="1" />
+          </span>
           <div>
-            <h1 class="font-medium">Dave Njoroge</h1>
-            <p class="text-sm">0707420685 | example@email.com</p>
+            <h1 class="font-medium font-secondary text-lg">
+              {{ document.user.name }}
+            </h1>
+            <p class="text-sm text-gray-600">{{ document.user.email }}</p>
           </div>
         </div>
       </div>
@@ -46,11 +61,14 @@
       </p>
       <hr />
       <div>
-
         <form @submit.prevent="createRemark">
           <div class="space-y-4">
-            <form-text placeholder="What are your thoughts on this document?" v-model="comment" required
-              :error="remarkStore.error?.comment"></form-text>
+            <form-text
+              placeholder="What are your thoughts on this document?"
+              v-model="comment"
+              required
+              :error="remarkStore.error?.comment"
+            ></form-text>
             <base-button type="submit" :loading>
               <span>Comment</span>
             </base-button>
@@ -59,48 +77,57 @@
 
         <div class="py-5">
           <template v-if="remarks.length">
-            <h1 class="font-medium">
-              Other remarks
-            </h1>
+            <h1 class="font-medium">Other remarks</h1>
             <div class="mt-3 space-y-2">
-              <div v-for="remark in remarks" :key="remark.id" class="border rounded-xl p-4">
+              <div
+                v-for="remark in remarks"
+                :key="remark.id"
+                class="border rounded-xl p-4"
+              >
                 <div class="flex">
-                  <div class="flex-auto">
+                  <div class="flex-auto flex flex-col space-y-2">
+                    <div class="flex space-x-2 items-center font-secondary">
+                      <p>{{ remark.user.name }}</p>
+                      <span>|</span>
+                      <p class="text-sm text-gray-500">
+                        {{ parseDate(remark.created_at) }}
+                      </p>
+                    </div>
                     <h1 class="text-lg">
                       {{ remark.content }}
                     </h1>
-                    <p class="text-sm text-gray-500">
-                      {{ parseDate(remark.created_at) }}
-                    </p>
                   </div>
-                  <button v-if="remark.user_id === auth.user?.id" type="button" @click="deleteComment(remark.id)">
+                  <button
+                    v-if="remark.user_id === auth.user?.id"
+                    type="button"
+                    @click="deleteComment(remark.id)"
+                  >
                     <Trash2 />
                   </button>
                 </div>
               </div>
             </div>
           </template>
-          <p v-else class="text-slate-500 text-center ">
+          <p v-else class="text-slate-500 text-center">
             No remarks. Be the first to leave one.
           </p>
         </div>
       </div>
     </div>
-
   </base-modal>
 </template>
 <script lang="ts" setup>
 import { useAuthStore } from "@/stores/auth"
 import { useDocumentStore, type Document } from "@/stores/docs"
-import { useRemarkStore, type Remark } from "@/stores/remarks";
-import BaseModal from '@/components/base/modal.vue';
-import BaseAlert from '@/components/base/alert.vue';
-import FormText from '@/components/form/text.vue'
-import BaseButton from "@/components/base/button.vue";
+import { useRemarkStore, type Remark } from "@/stores/remarks"
+import BaseModal from "@/components/base/modal.vue"
+import BaseAlert from "@/components/base/alert.vue"
+import FormText from "@/components/form/text.vue"
+import BaseButton from "@/components/base/button.vue"
 import { computed, onMounted, ref, watch } from "vue"
-import { Trash2 } from "lucide-vue-next";
-import parseDate from "@/utils/parseDate";
-import BaseLoader from "@/components/base/loader.vue";
+import { Trash2, User2 } from "lucide-vue-next"
+import parseDate from "@/utils/parseDate"
+import BaseLoader from "@/components/base/loader.vue"
 
 const props = defineProps<{
   document: Document
@@ -108,26 +135,26 @@ const props = defineProps<{
 }>()
 
 const model = defineModel<boolean>()
-const emit = defineEmits(["close", 'open-document'])
+const emit = defineEmits(["close", "open-document"])
 const store = useDocumentStore()
 const remarkStore = useRemarkStore()
 const auth = useAuthStore()
-const comment = ref('')
+const comment = ref("")
 const remarks = ref<Remark[]>([])
 
-const loading = computed(() => remarkStore.loading);
+const loading = computed(() => remarkStore.loading)
 
 const createRemark = async () => {
-  console.log('create');
-  if (comment.value === '') {
+  console.log("create")
+  if (comment.value === "") {
     return
   }
 
-  const _newRemark = await remarkStore.create(props.document.id, comment.value);
+  const _newRemark = await remarkStore.create(props.document.id, comment.value)
   if (_newRemark) {
-    remarks.value.push(_newRemark);
+    remarks.value.push(_newRemark)
 
-    comment.value = ''
+    comment.value = ""
   }
 }
 
@@ -148,31 +175,30 @@ const deleteDocument = async (id: number) => {
 }
 
 const openDocument = (url: string, id: number) => {
-  emit('open-document', { url, id })
+  emit("open-document", { url, id })
 }
 
 const getComments = async () => {
-  const _r = await remarkStore.getRemarks(props.document.id);
+  const _r = await remarkStore.getRemarks(props.document.id)
   if (_r) {
-    remarks.value = _r;
+    remarks.value = _r
   }
 }
 
 const deleteComment = async (id: number) => {
-
-  if (!confirm("Are you sure you want to delete this comment?",)) {
-    return;
+  if (!confirm("Are you sure you want to delete this comment?")) {
+    return
   }
 
-  const success = await remarkStore.destroy(id);
+  const success = await remarkStore.destroy(id)
   if (success) {
-    remarks.value = remarks.value.filter(r => r.id !== id)
+    remarks.value = remarks.value.filter((r) => r.id !== id)
   }
 }
 
 const close = () => {
   model.value = false
-  emit('close')
+  emit("close")
 }
 
 watch(comment, () => {
@@ -184,5 +210,4 @@ onMounted(getComments)
 onMounted(() => {
   // active.value = props.show
 })
-
 </script>
--- src/components/home/documents.vue
+++ src/components/home/documents.vue
@@ -19,8 +19,8 @@
     <table class="table w-full documents-table mt-6 md:mt-10">
       <tbody>
         <tr v-for="(row, index) in documents" :key="index">
-          <td class="md:w-24">
-            <div class="flex justify-center w-full">
+          <td class="md:w-32">
+            <div class="flex justify-center w-full px-4">
               <span
                 class="h-16 w-16 border rounded-full flex items-center justify-center"
               >
@@ -29,27 +29,40 @@
             </div>
           </td>
           <td>
-            <div class="flex items-center justify-between">
-              <h1 class="font-medium text-lg md:text-2xl font-secondary">
-                {{ row.name }}
-              </h1>
-              <document-status-tag v-if="row.is_draft"
-                >draft</document-status-tag
+            <div class="space-y-2">
+              <div
+                class="flex items-center justify-between md:justify-start md:space-x-3"
               >
-              <document-status-tag status="success" v-else
-                >published</document-status-tag
+                <h1 class="font-medium text-lg md:text-3xl font-secondary">
+                  {{ row.name }}
+                </h1>
+                <document-status-tag v-if="row.is_draft"
+                  >draft</document-status-tag
+                >
+                <document-status-tag status="success" v-else
+                  >published</document-status-tag
+                >
+              </div>
+              <div class="flex items-center space-x-2">
+                <p class="font-light">
+                  by <span class="font-medium">{{ row.user.name }}</span>
+                </p>
+                <span>&ndash;</span>
+                <p class="text-sm text-gray-500">
+                  {{ parseDate(row.created_at) }}
+                </p>
+              </div>
+              <p
+                class="text-sm max-w-xl line-clamp-2 font-light"
+                v-if="row.details"
               >
+                {{ row.details }}
+              </p>
             </div>
-            <p
-              class="text-sm max-w-xl mt-1 line-clamp-2 font-light"
-              v-if="row.details"
-            >
-              {{ row.details }}
-            </p>
             <div class="flex gap-3 flex-wrap items-center flex-auto py-4">
               <button
                 type="button"
-                class="border rounded-lg px-2 py-1 inline-flex items-center leading-normal space-x-2 text-sm"
+                class="border rounded-lg px-2 py-1 inline-flex items-center leading-normal space-x-2 text-sm font-secondary"
                 @click="viewMoreInfo(row.id)"
               >
                 <span>More info</span>
@@ -57,17 +70,23 @@
               </button>
               <button
                 type="button"
-                class="border rounded-lg px-2 py-1 inline-flex items-center leading-normal space-x-2 text-sm"
+                class="border rounded-lg px-2 py-1 inline-flex items-center leading-normal space-x-2 text-sm font-secondary"
                 :loading="loadingDocument === row.id"
                 @click="openDocument({ path: row.url, id: row.id })"
               >
                 <span>View document</span>
                 <ExternalLink :size="20" :stroke-width="1.5" />
               </button>
+              <div
+                v-if="row.remarks[0] && row.remarks[0].count"
+                class="span inline-flex items-center space-x-2 text-sm font-secondary font-medium py-1 px-2"
+              >
+                <MessageCircle :size="20" :stroke-width="1.5" />
+                <span class="">
+                  {{ row.remarks[0].count }}
+                </span>
+              </div>
             </div>
-            <p class="mt-1 text-sm text-gray-500">
-              Created on {{ parseDate(row.created_at) }}
-            </p>
           </td>
         </tr>
       </tbody>
@@ -92,7 +111,13 @@ import BaseButton from "@/components/base/button.vue"
 import DocumentView from "@/components/home/document-view.vue"
 import DocumentStatusTag from "@/components/home/document-status-tag.vue"
 import DocumentsEmpty from "@/components/home/documents-empty.vue"
-import { ExternalLink, Info, FileText, Plus } from "lucide-vue-next"
+import {
+  ExternalLink,
+  Info,
+  FileText,
+  Plus,
+  MessageCircle,
+} from "lucide-vue-next"
 import { useAuthStore } from "@/stores/auth"
 import { useDocumentStore } from "@/stores/docs"
 import { computed, ref } from "vue"
