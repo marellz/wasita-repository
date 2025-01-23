40beccf4fb383afb686f8683936996d55f1c95f7
Dave Njoroge <davenjoroge10@gmail.com>
2025-01-23 22:32:55 +0300

    validation for document form

--- src/components/docs/form.vue
+++ src/components/docs/form.vue
@@ -1,42 +1,87 @@
 <template>
-  <form @submit.prevent="submit">
+  <base-alert
+    class="mb-10"
+    v-if="store.errors && Object.keys(store.errors).length"
+    variant="error"
+    title="Document form errors"
+  >
+    {{ store.errors }}
+  </base-alert>
+
+  <Form @submit="submit">
     <div class="space-y-4">
-      <form-input label="Document name" v-model="_form.name" :error="errors.name" required />
-      <form-text rows="5" label="Document details" v-model="_form.details" :error="errors.details" required />
+      <form-input
+        label="Document name"
+        v-model="name"
+        :error="errors.name"
+        required
+      />
+      <form-text
+        rows="5"
+        label="Document details"
+        v-model="details"
+        :error="errors.details"
+        required
+      />
       <div class="!my-10 space-y-4">
         <base-alert v-if="isUpdate" title="Please note">
-          <p>Inputting a new file will mean that the original file will be replaced</p>
+          <p>
+            Inputting a new file will mean that the original file will be
+            replaced
+          </p>
         </base-alert>
-        <document-input v-model="file" :error="errors.document"></document-input>
+        <document-input v-model="file" :error="errors.file"></document-input>
       </div>
       <fieldset class="border rounded-xl p-4">
-        <legend class="px-4"><form-label class="!mb-0">Document settings</form-label></legend>
+        <legend class="px-4">
+          <form-label class="!mb-0">Document settings</form-label>
+        </legend>
         <div class="space-y-4">
-          <form-checkbox label="Save as draft" v-model="_form.is_draft" @change="handleDraftStatus"></form-checkbox>
-          <form-checkbox label="Public document" v-model="_form.is_public" :disabled="_form.is_draft"></form-checkbox>
-          <form-checkbox label="Add collaborators" v-model="hasCollaborators" @change="handleCollaborationChange"
-            :disabled="!!_form.collaborators?.length"></form-checkbox>
+          <form-checkbox
+            label="Save as draft"
+            v-model="is_draft"
+            @change="handleDraftStatus"
+          ></form-checkbox>
+          <form-checkbox
+            label="Public document"
+            v-model="is_public"
+            :disabled="is_draft"
+          ></form-checkbox>
+          <form-checkbox
+            label="Add collaborators"
+            v-model="hasCollaborators"
+            @change="handleCollaborationChange"
+            :disabled="!!collaborators?.length"
+          ></form-checkbox>
         </div>
       </fieldset>
       <fieldset class="border rounded-xl p-4" v-if="hasCollaborators">
-        <legend class="px-4"><form-label class="!mb-0">Collaborators</form-label></legend>
+        <legend class="px-4">
+          <form-label class="!mb-0">Collaborators</form-label>
+        </legend>
         <div class="space-y-4">
-          <template v-if="_form.collaborators?.length">
+          <template v-if="collaborators?.length">
             <p class="font-medium">Added collaborators</p>
             <div class="flex flex-wrap gap-3">
-              <div v-for="(item, index) in displayedCollaborators" :key="index"
-                class="flex items-center space-x-4 border rounded-lg p-2">
+              <div
+                v-for="(item, index) in displayedCollaborators"
+                :key="index"
+                class="flex items-center space-x-4 border rounded-lg p-2"
+              >
                 <div class="flex-auto">
                   <p class="font-medium">
-                    {{ item.name || 'No name' }}
+                    {{ item.name || "No name" }}
                   </p>
                   <p class="text-xs text-slate-600">
                     {{ item.email }}
                   </p>
                 </div>
                 <div class="flex-none">
-                  <button type="button" class="rounded-full p-1 hover:bg-red-100 hover:text-red-500"
-                    @click="removeSelectedCollaborator(index)">
+                  <button
+                    type="button"
+                    class="rounded-full p-1 hover:bg-red-100 hover:text-red-500"
+                    @click="removeSelectedCollaborator(index)"
+                  >
                     <X />
                   </button>
                 </div>
@@ -46,20 +91,31 @@
 
           <p v-else class="text-slate-500">None selected</p>
 
-          <form-input v-model="userQuery" placeholder="Search for users"></form-input>
+          <form-input
+            v-model="userQuery"
+            placeholder="Search for users"
+          ></form-input>
 
           <div v-if="userStore.loading" class="py-10 text-center">
             <base-loader></base-loader>
           </div>
 
           <template v-else>
-            <hr>
+            <hr />
             <div class="py-4 pt-2 flex flex-wrap gap-3">
-              <form-checkbox v-for="(item, index) in displayedUsers" :key="index"
+              <form-checkbox
+                v-for="(item, index) in displayedUsers"
+                :key="index"
                 class="flex items-center space-x-4 border rounded-lg px-4 py-2 border-slate-200"
-                :class="{ 'bg-slate-200': item.email === auth.user?.email, '!border-indigo-600': _form.collaborators?.includes(item.id) }"
-                v-model="_form.collaborators" :value="item.id" name="collaborators"
-                :disabled="item.email === auth.user?.email">
+                :class="{
+                  'bg-slate-200': item.email === auth.user?.email,
+                  '!border-indigo-600': collaborators?.includes(item.id),
+                }"
+                v-model="collaborators"
+                :value="item.id"
+                name="collaborators"
+                :disabled="item.email === auth.user?.email"
+              >
                 <div class="ml-4">
                   <p class="font-medium">
                     {{ item.name || `&ndash; &ndash;` }}
@@ -75,18 +131,32 @@
       </fieldset>
 
       <fieldset class="border rounded-xl p-4">
-        <legend class="px-4"><form-label class="!mb-0">Categorization</form-label></legend>
+        <legend class="px-4">
+          <form-label class="!mb-0">Categorization</form-label>
+        </legend>
         <div class="space-y-4">
-          <form-select label="Category" v-model="_form.category">
-            <option v-for="item in categories" :value="item.value" :key="item.value">
+          <form-select
+            label="Category"
+            v-model="category"
+            :error="errors.category"
+          >
+            <option
+              v-for="item in categoriesList"
+              :value="item.value"
+              :key="item.value"
+            >
               {{ item.label }}
             </option>
           </form-select>
           <div>
             <form-label>Tags</form-label>
             <div class="flex gap-4">
-              <div v-for="(item, index) in tags" :key="index">
-                <form-checkbox :value="item.value" :label="item.label" v-model="_form.tags" />
+              <div v-for="(item, index) in tagsList" :key="index">
+                <form-checkbox
+                  :value="item.value"
+                  :label="item.label"
+                  v-model="tags"
+                />
               </div>
             </div>
           </div>
@@ -99,55 +169,98 @@
         </base-button>
       </div>
     </div>
-  </form>
+  </Form>
 </template>
 <script lang="ts" setup>
-
-import BaseLoader from '@/components/base/loader.vue';
-import BaseAlert from '@/components/base/alert.vue';
-import BaseButton from '@/components/base/button.vue';
-import FormInput from '@/components/form/input.vue';
-import FormLabel from '@/components/form/label.vue';
-import FormText from '@/components/form/text.vue';
-import FormSelect from '@/components/form/select.vue';
-import FormCheckbox from '@/components/form/checkbox.vue';
-import DocumentInput from '@/components/form/document.vue';
-import { type DocumentForm, useDocumentStore } from '@/stores/docs';
-import { computed, onMounted, ref } from 'vue';
-import { useUserStore, type User } from '@/stores/users';
-import { useAuthStore } from '@/stores/auth';
-import { X } from 'lucide-vue-next';
+import BaseLoader from "@/components/base/loader.vue"
+import BaseAlert from "@/components/base/alert.vue"
+import BaseButton from "@/components/base/button.vue"
+import FormInput from "@/components/form/input.vue"
+import FormLabel from "@/components/form/label.vue"
+import FormText from "@/components/form/text.vue"
+import FormSelect from "@/components/form/select.vue"
+import FormCheckbox from "@/components/form/checkbox.vue"
+import DocumentInput from "@/components/form/document.vue"
+import { type DocumentForm, useDocumentStore } from "@/stores/docs"
+import { computed, onMounted, ref } from "vue"
+import { useUserStore, type User } from "@/stores/users"
+import { useAuthStore } from "@/stores/auth"
+import { Form, useForm } from "vee-validate"
+import { X } from "lucide-vue-next"
+import * as yup from "yup"
 
 export interface DocumentFormPayload {
-  data: DocumentForm,
+  data: DocumentForm
   file: File | null
 }
 
-const props = withDefaults(defineProps<{
-  form?: DocumentForm;
-}>(), {
+const props = withDefaults(
+  defineProps<{
+    form?: DocumentForm
+  }>(),
+  {},
+)
+
+const isUpdate = computed(() => props.form?.id)
+
+const newForm = {
+  name: "",
+  details: "",
+  url: null,
+  is_draft: false,
+  is_public: false,
+  category: null,
+  tags: [],
+  collaborators: [],
+  file: null,
+}
+
+const yupFileSChema = isUpdate.value
+  ? yup.object(File).nullable()
+  : yup.object(File).required("File is required")
+
+const schema = yup.object({
+  name: yup.string().required("Document requires a name"),
+  details: yup.string().required("Document information is required"),
+  is_draft: yup.boolean(),
+  is_public: yup.boolean(),
+  category: yup.string().required(),
+  tags: yup.array().of(yup.string()),
+  collaborators: yup.array().of(yup.string()),
+  file: yupFileSChema,
 })
 
-const emit = defineEmits(['submit'])
+const { errors, defineField, resetForm, handleSubmit } = useForm({
+  validationSchema: schema,
+})
+
+const [name] = defineField("name")
+const [details] = defineField("details")
+const [is_draft] = defineField("is_draft")
+let [is_public] = defineField("is_public")
+const [category] = defineField("category")
+const [collaborators] = defineField("collaborators")
+const [tags] = defineField("tags")
+const [file] = defineField("file")
+
+const emit = defineEmits(["submit"])
 
 const store = useDocumentStore()
 const userStore = useUserStore()
 const auth = useAuthStore()
 
 const loading = computed(() => store.loadingSingle)
-const errors = computed(() => store.errors)
-const isUpdate = computed(() => props.form?.id)
-const _form = ref({ ...props.form })
-const file = ref<File | undefined>()
 
 const hasCollaborators = ref(false)
 const users = ref<User[]>([])
-const userQuery = ref<string>('')
+const userQuery = ref<string>("")
 
 const displayedUsers = computed(() => {
-  const r = users.value.filter(u => {
+  const r = users.value.filter((u) => {
     const v = userQuery.value.toLowerCase()
-    return u.name?.toLowerCase().includes(v) || u.email.toLowerCase().includes(v)
+    return (
+      u.name?.toLowerCase().includes(v) || u.email.toLowerCase().includes(v)
+    )
   })
 
   if (userQuery.value.length) {
@@ -158,50 +271,45 @@ const displayedUsers = computed(() => {
 })
 
 const displayedCollaborators = computed(() => {
-  if (!_form.value.collaborators) {
+  if (!collaborators) {
     return []
   }
 
-  return users.value.filter(u => _form.value.collaborators?.includes(u.id))
+  return users.value.filter((u) => collaborators?.includes(u.id))
 })
 
 const removeSelectedCollaborator = (index: number) => {
-  _form.value.collaborators?.splice(index, 1)
+  collaborators?.splice(index, 1)
 }
 
 const getUsers = async () => {
-  const _u = await userStore.getUsers();
+  const _u = await userStore.getUsers()
 
   if (_u) {
     users.value = _u
   }
 }
 
+const submit = handleSubmit((values) => {
+  emit("submit", { data: values, file: values.file })
+})
 
-
-const submit = () => {
-  const _file = file.value ? file.value : null
-  emit('submit', { data: _form.value, file: _file })
-}
-
-const tags = ref([
+const tagsList = ref([
   { label: "Finance", value: "finance" },
   { label: "Meeting", value: "meeting" },
   { label: "2025", value: "2025" },
 ])
 
-
-const categories = ref<{ label: string, value: string }[]>([
+const categoriesList = ref<{ label: string; value: string }[]>([
   { label: "General", value: "general" },
   { label: "Financial", value: "financial" },
   { label: "Minutes", value: "minutes" },
   { label: "Contracts", value: "contracts" },
 ])
 
-
 const handleDraftStatus = () => {
-  if (_form.value.is_draft) {
-    _form.value.is_public = false
+  if (is_draft) {
+    is_public = false
   }
 }
 
@@ -212,10 +320,15 @@ const handleCollaborationChange = async (v: boolean) => {
 }
 
 onMounted(async () => {
-  if (props.form?.collaborators?.length) {
+  if (props.form && props.form.id) {
+    resetForm({ values: props.form })
+  } else {
+    resetForm({ values: newForm })
+  }
+
+  if (collaborators.length) {
     hasCollaborators.value = true
     await getUsers()
   }
 })
-
 </script>
